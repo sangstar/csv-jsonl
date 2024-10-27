e97c46fff0051444730733a7595795242e90e203
Sanger Steel <sangersteel@gmail.com>
2024-10-27 18:18:51 -0400

    fix: Allow `-n` newline CLI arg, fix `&&` operators

--- main.c
+++ main.c
@@ -39,7 +39,7 @@ int is_float(char *string) {
         }
         counter += isnumber(string[i]);
     }
-    if ((counter == strlen(string) - 1) & has_dot) {
+    if ((counter == strlen(string) - 1) && has_dot) {
         return 1;
     }
     return 0;
@@ -48,32 +48,38 @@ int is_float(char *string) {
 // Creates a sliced_buffer from a text buffer `buf`
 // A sliced_buffer is a slice of text that guarantees
 // terminating with a newline
-sliced_buffer *get_to_newline(char *buf, int cursor){
+sliced_buffer *get_to_newline(char *buf, int cursor, char *newline_seq){
     sliced_buffer *sliced = (sliced_buffer *)malloc(sizeof(sliced_buffer));
     sliced->slice = (char *)malloc(sizeof(char) * strlen(buf));
     sliced->cursor = 0;
     sliced->has_newline = 0;
 
-    // Skip past the previous \r\n
+    // Skip past the previous newline_seq
     if (cursor != 0) {
         cursor+=2;
     }
 
     for (int i = cursor; i < strlen(buf); ++i) {
-        if (buf[i] == *"\r\n"){
-            sliced->has_newline = 1;
-            break;
-        } else {
 
-            // Ensure the indexing is positive based on the relative
-            // sizes of the cursor and iterator
-            if (cursor > i) {
-                sliced->slice[cursor - i] = buf[i];
-            } else {
-                sliced->slice[i - cursor] = buf[i];
+        // Check if the search window for a newline_seq is within bounds
+        if (buf[i] == newline_seq[0] && i + strlen(newline_seq) < strlen(buf)) {
+            int comparator = 0;
+            for (int j = 0; j < strlen(newline_seq); ++j) {
+                comparator+= buf[i + j] == newline_seq[j];
             }
-
+            if (comparator == strlen(newline_seq)) {
+                sliced->has_newline = 1;
+                break;
+            }
         }
+        // Ensure the indexing is positive based on the relative
+        // sizes of the cursor and iterator
+        if (cursor > i) {
+            sliced->slice[cursor - i] = buf[i];
+        } else {
+            sliced->slice[i - cursor] = buf[i];
+        }
+
     }
 
     // Shift the cursor by the amount moved. This actually refers to
@@ -83,7 +89,7 @@ sliced_buffer *get_to_newline(char *buf, int cursor){
 }
 
 // Writes lines of text from a `buf` buffer to a `lines_buffer`
-char *add_lines_from_buffer(lines_buffer *line_buf, char *buf) {
+char *add_lines_from_buffer(lines_buffer *line_buf, char *buf, char *newline_seq) {
 
     // A separate cursor variable is maintained that keeps track of
     // sliced->cursor values to avoid having to have `get_to_newline`
@@ -95,7 +101,7 @@ char *add_lines_from_buffer(lines_buffer *line_buf, char *buf) {
 
 
     while (cursor < strlen(buf)) {
-        sliced_buffer *sliced = get_to_newline(buf, cursor);
+        sliced_buffer *sliced = get_to_newline(buf, cursor, newline_seq);
         cursor = sliced->cursor;
 
         // If the slice is a complete row, save it
@@ -117,7 +123,7 @@ char *add_lines_from_buffer(lines_buffer *line_buf, char *buf) {
 
 // Saves each line in a CSV to a `lines_buffer` struct
 // Reads up to NUM_LINES lines
-lines_buffer *file_to_lines_buffer(FILE *file) {
+lines_buffer *file_to_lines_buffer(FILE *file, char *newline_seq) {
 
     lines_buffer *lines = (lines_buffer *)malloc(sizeof(lines_buffer) * NUM_LINES);
     lines->lines = (char **)malloc(sizeof(char) * NUM_LINES * BYTES_PER_BUFFER);
@@ -130,7 +136,7 @@ lines_buffer *file_to_lines_buffer(FILE *file) {
 
 
         // Add current buffer to lines and get overflow from previous call
-        char *overflow = add_lines_from_buffer(lines, buf);
+        char *overflow = add_lines_from_buffer(lines, buf, newline_seq);
 
         // Clear buffer first, since buf has its sized changed dynamically based on overflow, so
         // reading BYTES_PER_BUFFER bytes into the buffer with a dynamic buffer size of BYTES_PER_BUFFER + overflow_size
@@ -247,7 +253,7 @@ void lines_to_jsonl(lines_buffer *lines, char *outfile) {
             int col_value_idx = 0;
             for (int m = 0; m < max_col_name_length; ++m) {
                 int incrementor = m + counter;
-                if (!inside_outer_quotes & current_line[incrementor] == ',') {
+                if (!inside_outer_quotes && current_line[incrementor] == ',') {
                     counter += m + 1;
                     break;
                 }
@@ -290,7 +296,7 @@ void lines_to_jsonl(lines_buffer *lines, char *outfile) {
 int main(int argc, char *argv[]) {
     char *input_filename = argv[1];
     char *output_filename = argv[2];
-    if (argc > 3) {
+    if (argc < 3) {
         printf("Error: Missing required arguments.\n\n");
         printf("Usage: ./csv-jsonl input_csv output_jsonl\n");
         printf("Arguments:\n");
@@ -299,12 +305,32 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    char *newline_seq = "\n";
+    for (int i = 0; i < argc; ++i) {
+        if (i + 1 < argc && strcmp(argv[i], "-n") == 0) {
+
+            // CLI args are literally parsed string,
+            // so it won't automatically identify
+            // special characters
+
+            // I've only directly caught two basic
+            // newline_seq cases here
+            if (strcmp(argv[i + 1], "\\r\\n") == 0) {
+                newline_seq = "\r\n";
+            } else if (strcmp(argv[i + 1], "\\n") == 0) {
+                newline_seq = "\n";
+            } else {
+                newline_seq = argv[i + 1];
+            }
+        }
+    }
+
     FILE *file = fopen(input_filename, "r");
     if (file == NULL) {
         printf("Could not read input file");
         exit(1);
     }
-    lines_buffer *lines = file_to_lines_buffer(file);
+    lines_buffer *lines = file_to_lines_buffer(file, newline_seq);
     lines_to_jsonl(lines, output_filename);
     free(lines);
     return 0;
